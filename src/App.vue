ce9a4114d2989e50607466e61e3b25ecb4a75b2e
Frankyfu <1286760898@qq.com>
2020-08-02 20:14:56 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="landing-page">
+  <div id="app">
+    <img src="./assets/timg.jpg" id="logo">
+    <router-view/>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#logo{
+  margin-top:60px;
+  border-radius: 50px;
+  border: 2px solid rgb(255, 255, 255) ;
+  height: auto;
+  width: 100px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  margin-top: 50px;
+}
+.landing-page{
+  content:"";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: url(./assets/background.jpg)no-repeat;
+  background-size: cover;
+}
+</style>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <h1>Welcome to Franky's blog</h1>
+      <div id="content">
+        <h2 id="acount">Acount:<input v-model="user.acount"  type="text"  ></h2>
+        <h2>Password:<input v-model="user.password"  type = "password" ></h2>
+      </div>
+      <a v-on:click="SignIn" href="#" >Sign in</a>
+      <h1>{{acount}}
+      {{password}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignIn',
+  data () {
+    return {
+      user: {
+        password: '',
+        acount: ''
+      }
+    }
+  },
+  methods:{
+    SignIn:function(){
+      
+      this.$http.post("",{
+        acount:this.user.acount,
+        password:this.user.password
+      }).then(function(data){
+        console.log(data)
+      })
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,h2 {
+  font-weight: normal;
+}
+#acount{
+  margin-left: 30px;
+}
+input{
+  background-color: white;
+  border: solid 2px black;
+  height: 25px;
+  width: 200px;
+  font-size: 20px;
+  margin-left: 10px;
+}
+a{
+  font-size: 23px;
+  color: black;
+  display: inline-block;
+  text-decoration: none ;
+  border: solid 2px black;
+  padding:3px;
+  border-radius: 4px;
+}
+</style>
